7a3f1439b49a0d8235ce504e7502bebb8d30ce1b
getorey <75866437+getorey@users.noreply.github.com>
2026-06-17 13:15:26 +0900

    fix: remove basePath for custom domain (workhub-landing.koavio.com)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/workhub-landing' : '',
-  images: {
-    unoptimized: true,
-  },
+    output: 'export',
+    basePath: '',
+    images: {
+          unoptimized: true,
+    },
 };
 
 export default nextConfig;
